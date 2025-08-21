f9926de16b66a65143fbd97a0a60d4022a8a9010
Allan Uy <allan@etgrow.com>
2025-08-21 18:54:35 -0500

    att4

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/terms" element={<Terms />} />
--- vite.config.ts
+++ vite.config.ts
@@ -2,24 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
-
-// // https://vitejs.dev/config/
-// export default defineConfig(({ mode }) => ({
-//   server: {
-//     host: "::",
-//     port: 8080,
-//   },
-//   plugins: [
-//     react(),
-//     mode === 'development' &&
-//     componentTagger(),
-//   ].filter(Boolean),
-//   resolve: {
-//     alias: {
-//       "@": path.resolve(__dirname, "./src"),
-//     },
-//   },
-// }));
+import { ghPages } from "vite-plugin-gh-pages";
 
 export default defineConfig(({ mode }) => ({
   server: {
@@ -30,11 +13,12 @@ export default defineConfig(({ mode }) => ({
     react(),
     mode === 'development' &&
     componentTagger(),
+    ghPages(),
   ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: '' //mode === 'development' ? '/' : '/zenyai-receipt-magic/'
+  base: mode === 'development' ? '/' : '/zenyai-receipt-magic',
 }));
